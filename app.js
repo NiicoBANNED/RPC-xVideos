7bc31c5e27ced5be46fae8062191017bbae0a7c4
NiicoBANNED <77835624+NiicoBANNED@users.noreply.github.com>
2021-05-24 21:49:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ require('dotenv').config();
 (async () => {
     client.on('ready', async () => {
         await client.setActivity({ // Setting the Rich Presence Activity based on what is passed in here.
-            details: "Categoria: Tia y Sobrino",
-            state: "Video: Tia calentona coquetea con su sobrino y cogen delatante de su marido",
+            details: "Categoria: Stepsister",
+            state: "Video: Stepsis got stuck and I finally fucked her tight ass and wet pussy",
             startTimestamp: new Date(),
             largeImageKey: "xvideos",
             largeImageText: "xvideos.com"
         }).catch(err => console.log(err));
 
-        console.log("Discord Rich Presence has been enabled.");
+        console.log("xVideos.com is start");
     });
 
     await client.login({ clientId: process.env.applicationID }).catch(console.error);
